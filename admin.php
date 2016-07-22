d27e3fbee337a16fde7b1677015bf1c35409f318
Pascal Sommer <p@pascalsommer.ch>
2016-07-22 02:11:09 +0200

    working communication, no sql yet

--- admin.php
+++ admin.php
@@ -1,6 +1,7 @@
 <?php
 
-$sql_username = "";
+$sql_hostname = "localhost";
+$sql_username = "root";
 $sql_password = "";
 $sql_database = "pascalsommer_ch";
 
--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 // set session variable "access_granted" to 1
 //
 
-
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 $target_address = "http://example.com";
@@ -17,30 +19,99 @@ function redirect($address){
 	die();
 }
 
-if($_SESSION['access_granted']==1){
+if(isset($_SESSION["access_granted"]) && $_SESSION["access_granted"]==1){
 	redirect($target_address);
 }
 
 include('admin.php');
-include('phpseclib/Crypt/RSA.php');
+
+set_include_path(get_include_path() . PATH_SEPARATOR . "phpseclib");
+include('Crypt/RSA.php');
+$sql_tablename = "users";
+
+$rsa=new Crypt_RSA();
+
 
 if(!isset($_SESSION['private'])){
-	$rsa=new Crypt_RSA();
+	
 	extract($rsa->createKey());
 	$_SESSION["private"] = $privatekey;
 	$_SESSION["public"] = $publickey;
 }
+
+$rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+
+if(isset($_POST["cusr"]) && isset($_POST["cpwd"])){
+	$cusr = $_POST["cusr"];
+	$cpwd = $_POST["cpwd"];
+	$rsa->loadkey($_SESSION["private"]);
+
+
+	$pusr = $rsa->decrypt(base64_decode($cusr));
+	$ppwd = $rsa->decrypt(base64_decode($cpwd));
+
+	$sql = new mysqli($sql_hostname, $sql_username, $sql_password, $sql_database);
+	$stm = $sql->prepare("select hash from {$sql_tablename} where username = ?");
+	$stm->bind_param("s", $pusr);
+	$stm->execute();
+	$stm->bind_result($hash);
+	$stm->fetch();
+	
+	$login_ok = password_verify($ppwd, $hash);	
+	
+	echo $ppwd . "<br>";
+
+	$stm->close();
+
+}
+
 
 ?>
 
 <html>
 <head>
 <title>Login</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="jsencrypt.js"></script>
 </head>
 <body>
-<div id='publickey' class='hidden'><?= $_SESSION['public'] ?></div>
 
-asdf
+<style type="text/css">
+
+body{
+	text-align: center;
+}
+
+</style>
+<script type="text/javascript">
+
+function onSend(){
+	var encrypt = new JSEncrypt();
+	encrypt.setPublicKey($('#public').val());
+
+	var cpwd = encrypt.encrypt($("#pwd").val());
+	$("#pwd").val(cpwd);
+
+	var cusr = encrypt.encrypt($("#usr").val());
+	$("#usr").val(cusr);
+
+	return true;
+}
+
+</script>
+
+
+<?php print("<textarea id='public' style='display:none'>".$_SESSION["public"]."</textarea>");?>
+
+<form method="post" onsubmit="return onSend()">
+<p>Username</p>
+<input id="usr" type="text" name="cusr">
+<p>Password</p>
+<input id="pwd" type="password" name="cpwd">
+<br>
+<br>
+<input type="submit" value="OK">
+</form>
 
 </body>
 </html>
